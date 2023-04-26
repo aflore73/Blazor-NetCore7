68fc6a264eced2983b3129bcb7350f0d9e8256be
Alejandro Florentin <aflorentin@sar.gruposoftland.com>
2023-04-26 16:08:46 -0300

    Oficina

---
+++ BlazorPeliculas/Client/Utilidades/UtilidadesString.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BlazorPeliculas.Client.Utilidades
+{
+    public class UtilidadesString
+    {
+        public static string Transformar(string Nombre) => Nombre.ToUpper();
+    }
+}
